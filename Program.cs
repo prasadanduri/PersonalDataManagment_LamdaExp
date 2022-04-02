5919fb4601707a8e844e01535fef2b015e1e2413
prasad <prasadanduri13gmail.com>
2022-04-02 19:38:12 +0530

    Retrive list of records of age between 13 to 18

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace PersonDataManagement
             List<Person> listPersonInCity = new List<Person>();
             AddRecords(listPersonInCity);
 
-            RetreivingTopTwoRecords_ForAgeIs_LessThanSixty(listPersonInCity);
+
+            Retrieve_Record_Age_bet_Thirteen_to_Eighteen(listPersonInCity);
         }
         private static void AddRecords(List<Person> listPersonInCity)
         {
@@ -25,10 +26,11 @@ namespace PersonDataManagement
             listPersonInCity.Add(new Person("203456881", "Meena", "17 Wardha, Maharashtra", 21));
             listPersonInCity.Add(new Person("203456882", "Rita", "62 ,Baramati, Maharashtra", 22));
             listPersonInCity.Add(new Person("203456883", "Geeta", "42 Pune, Maharashtra", 54));
+
         }
-        public static void RetreivingTopTwoRecords_ForAgeIs_LessThanSixty(List<Person> listPersonInCity)
+        public static void Retrieve_Record_Age_bet_Thirteen_to_Eighteen(List<Person> listPersonInCity)
         {
-            foreach (Person person in listPersonInCity.FindAll(x => (x.Age < 60)).Take(2).ToList())
+            foreach (Person person in listPersonInCity.FindAll(x => (x.Age >= 13 && x.Age < 18)).ToList())
             {
                 Console.WriteLine("Name : " + person.Name + "\t\tAge : " + person.Age);
             }
